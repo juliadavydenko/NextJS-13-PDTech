3927f8869ebb6c12a95d255bfef2e4f89f040e41
juliadavydenko <julietta12345@yahoo.com>
2023-06-10 16:45:41 +0200

    created several routes, navbar component and now started working with listofposts API fetching

---
+++ next13-p-tech/src/app/components/navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+
+
+export default function Navbar() {
+    return (
+        <div>
+            <Link href="/">Home</Link> 
+            <Link href="/about">About</Link> 
+            <Link href="/about/us">About Us</Link> 
+            <Link href="/about/someone">About Someone Else</Link> 
+        </div>
+    )
+}
