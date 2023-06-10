94fd73f8d3ff3dd6d60e9bee97e2863d02601cb3
juliadavydenko <julietta12345@yahoo.com>
2023-06-10 17:32:35 +0200

    added second API that gives 10 users list in the same file and route. Now I'm going to try API with rndm pics

--- next13-p-tech/src/app/listofposts/page.tsx
+++ next13-p-tech/src/app/listofposts/page.tsx
@@ -3,14 +3,25 @@ const getPostsData = async () => {
     return res.json();
 };
 
+const getUsersData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    return res.json();
+};
 
 export default async function ListOfPosts () {
-    const posts = await getPostsData();
+    const [posts, users] = await Promise.all([getPostsData(), getUsersData()]);
     return (
         <div>
             {posts.map((post: any) => {
                 return <p>{post.title}</p>;
             })}
+
+          <div>
+
+            {users.map((user: any) => {
+                 return <p>{user.name}</p>;
+            })}
+          </div>
         </div>
     );
 }
