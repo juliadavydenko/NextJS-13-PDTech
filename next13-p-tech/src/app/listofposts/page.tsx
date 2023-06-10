d677b482bc59b583374baed75bf8303fefe61afc
juliadavydenko <julietta12345@yahoo.com>
2023-06-10 17:56:36 +0200

    Added third API with random pics to check non-static. It works. In the same file we now have 3 apis working. Also modified the config file so that it accepts image domain

--- next13-p-tech/src/app/listofposts/page.tsx
+++ next13-p-tech/src/app/listofposts/page.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+
+
 const getPostsData = async () => {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
     return res.json();
@@ -8,10 +11,20 @@ const getUsersData = async () => {
     return res.json();
 };
 
+const getDogData = async () => {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    return res.json();
+};
+
+
 export default async function ListOfPosts () {
-    const [posts, users] = await Promise.all([getPostsData(), getUsersData()]);
+    const [posts, users, dog] = await Promise.all([
+        getPostsData(), 
+        getUsersData(),
+        getDogData()]);
     return (
         <div>
+            <Image src = {dog.message} alt = "dog" width={300} height={300} />
             {posts.map((post: any) => {
                 return <p>{post.title}</p>;
             })}
